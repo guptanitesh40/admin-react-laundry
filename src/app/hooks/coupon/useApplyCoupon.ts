b389ae67ad12bc384dd7ee627c66db6d9e6f3f86
mohit kheraliya <kheraliyamohit2004@gmail.com>
2024-11-11 11:13:43 -0500

    show retrived company and branch data in user table, add proper calculation of sub total and total in order form

--- src/app/hooks/coupon/useApplyCoupon.ts
+++ src/app/hooks/coupon/useApplyCoupon.ts
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
 
-const APPLY_COUPON_URL = `${import.meta.env.VITE_BASE_URL}/coupon/apply`;
+const APPLY_COUPON_URL = `${import.meta.env.VITE_BASE_URL}/admin/coupon/apply`;
 
 const useApplyCoupon = () => {
   const [loading, setLoading] = useState<boolean>(false);
 
-  const applyCoupon = async (user_id: number, sub_total: number, couponCode: string) => {
+  const applyCoupon = async (user_id: number, order_Total: number, coupon_Code: string) => {
     const token = localStorage.getItem("authToken");
     setLoading(true);
 
@@ -17,7 +17,7 @@ const useApplyCoupon = () => {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
-        body: JSON.stringify({ user_id, sub_total, couponCode }),
+        body: JSON.stringify({ user_id, order_Total, coupon_Code }),
       });
 
       const data = await response.json();
@@ -25,9 +25,8 @@ const useApplyCoupon = () => {
         toast.error(data.message, { position: "top-center" });
         return false;
       }
+      return data.data;
 
-      toast.success(data.message, { position: "top-center" });
-      return data.data.discountAmount;
     } catch {
       toast.error("Unable to apply coupon code!");
     } finally {
